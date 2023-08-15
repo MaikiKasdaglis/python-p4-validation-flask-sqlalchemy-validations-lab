65e3c29b4f6234ac34bcf7c142a8e01e5e4055ea
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-08-15 14:38:25 -0600

    finishes lab

--- server/models.py
+++ server/models.py
@@ -8,7 +8,18 @@ class Author(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Name cannot be empty')
+        return name
+    phone_number = db.Column(db.String(10))
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10: 
+            raise ValueError('must be 10 digits, bro')
+        return phone_number
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
@@ -22,8 +33,24 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be at least 250 characters long')
+        return content
     category = db.Column(db.String)
+    @validates('category')
+    def validate_category(self, key, category):
+        if category == "Fiction" or category == "Non-Fiction":
+            return category
+        else:
+            raise ValueError('must be Fiction of Non-Fiction. there is no grey area.')
     summary = db.Column(db.String)
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Summary length cannot exceed 250 characters')
+        return summary
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
